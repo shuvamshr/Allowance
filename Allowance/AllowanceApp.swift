de20d11623de1145f348720913191310ae299554
shuvamshr <shuvamshr@outlook.com>
2025-07-25 16:58:34 +1000

    Initial Commit

---
+++ Allowance/AllowanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllowanceApp.swift
+//  Allowance
+//
+//  Created by Shuvam Shrestha on 25/7/2025.
+//
+
+import SwiftUI
+
+@main
+struct AllowanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
